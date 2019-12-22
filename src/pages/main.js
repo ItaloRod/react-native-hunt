081d8d9900e4451246755f88bfb502ed3d8f644f
Paulo Italo <pauloitalo@hotmail.com.br>
2019-12-22 17:57:35 -0300

    finish react-native

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,15 +1,111 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
   static navigationOptions = {
     title: 'JsHunt',
   };
+
+  // States
+  state = {
+    productInfo: {},
+    docs: [],
+    page: 1,
+  };
+
+  // functions
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+    const {docs, ...productInfo} = response.data;
+    this.setState({docs: [...this.state.docs, ...docs], productInfo, page});
+  };
+
+  loadMore = () => {
+    const {page, productInfo} = this.state;
+
+    if (page === productInfo.pages) {
+      return;
+    }
+
+    const pageNumber = page + 1;
+    this.loadProducts(pageNumber);
+  };
+
+  // renders
+  renderItem = ({item}) => (
+    <View style={styles.productContainer}>
+      <Text style={styles.productTitle}>{item.title}</Text>
+      <Text styçe={styles.productDescription}>{item.description}</Text>
+      <TouchableOpacity
+        style={styles.productButton}
+        onPress={() => {
+          this.props.navigation.navigate('Product', {product: item});
+        }}>
+        <Text style={styles.productButtonText}>Acessar</Text>
+      </TouchableOpacity>
+    </View>
+  );
   render() {
     return (
-      <View>
-        <Text>Página Main</Text>
+      <View style={styles.container}>
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={this.state.docs}
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+          onEndReached={this.loadMore}
+          onEndReachedThreshold={0.1}
+        />
       </View>
     );
   }
 }
+// Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+  list: {
+    padding: 20,
+  },
+  productContainer: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+  },
+  productTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  productDescription: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24,
+  },
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#DA552F',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  productButtonText: {
+    fontSize: 16,
+    color: '#da552f',
+    fontWeight: 'bold',
+  },
+});
